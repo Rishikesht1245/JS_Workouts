294beb5a446b3da94080ba18d0411e52db636d96
Rishikesh T <rishikeshtgit@gmail.com>
2023-04-24 18:41:24 +0530

    Created a javascript file to split a string into an array

---
+++ unit_2/day_1/task_2.js
@@ -0,0 +1,8 @@
+//2.Write a JavaScript function to split a string and convert it into an array of words.
+
+function stringToArray(str){
+      console.log(str.split(" "));
+}
+
+let str = 'Rishi Kesh T';
+stringToArray(str);
