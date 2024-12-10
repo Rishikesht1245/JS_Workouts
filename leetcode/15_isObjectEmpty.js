2ea7956c6113e7728c8aba7c33159dcd49a736f7
Rishikeshsynosys <rishikesh@synoys.ae>
2024-12-10 16:59:40 +0530

    optimized the is empty function

--- leetcode/15_isObjectEmpty.js
+++ leetcode/15_isObjectEmpty.js
@@ -1,14 +1,18 @@
 function isEmpty(obj) {
-    if(typeof obj === "object" && !Array.isArray(obj)){
-        if(Object.keys(obj)?.length === 0){
-            return true;
-        }
-    }else if(Array.isArray(obj)){
-        if(obj?.length === 0){
-            return true;
-        }
-    }
-    return false;
+    // if(typeof obj === "object" && !Array.isArray(obj)){
+    //     if(Object.keys(obj)?.length === 0){
+    //         return true;
+    //     }
+    // }else if(Array.isArray(obj)){
+    //     if(obj?.length === 0){
+    //         return true;
+    //     }
+    // }
+    // return false;
+
+    // shorter way : type of array is object, on an arrya we can access the method Object.entries();
+
+    return Object.entries(obj).length === 0;
 };
 
 console.log(isEmpty({name : "rishi"}), "non object");
