18380af3d2dfd621a2cf7cca283b701cdf1acc58
Rishikeshsynosys <rishikesh@synoys.ae>
2024-12-10 16:56:57 +0530

    implemented is empty array or object

---
+++ leetcode/15_isObjectEmpty.js
@@ -0,0 +1,16 @@
+function isEmpty(obj) {
+    if(typeof obj === "object" && !Array.isArray(obj)){
+        if(Object.keys(obj)?.length === 0){
+            return true;
+        }
+    }else if(Array.isArray(obj)){
+        if(obj?.length === 0){
+            return true;
+        }
+    }
+    return false;
+};
+
+console.log(isEmpty({name : "rishi"}), "non object");
+console.log(isEmpty([]), "empty arr");
+console.log(isEmpty([1,2]), "non empty arr");
