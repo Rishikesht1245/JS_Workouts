c6bd11d65ac4da8e5b80ad3e8d6e7030120ea1a1
Rishikesht1245 <rishikeshtgit@gmail.com>
2024-12-10 17:53:35 +0530

    chunk array

---
+++ leetcode/16_chunk_arr.js
@@ -0,0 +1,12 @@
+function chunk(arr, size) {
+    const resArr = [];
+    for (let i = 0; i < arr.length; i+=size){
+    const tempArr = [...arr];
+    resArr.push(tempArr.slice(i,i+size))
+}
+
+return resArr;
+};
+
+
+console.log(chunk([1,2,4,5,6,7,6,8],2))
